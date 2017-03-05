f88fd3f522c0d0222bbfa2df68d9be2f2c5ca4f3
4blacktop <4blogga@gmail.com>
2017-03-05 20:34:29 +1000

    2034

--- www/js/index.js
+++ www/js/index.js
@@ -13,6 +13,6 @@
           "showFlipCameraButton" : true, // iOS and Android 
           "prompt" : "Place a barcode inside the scan area", // supported on Android only 
           "formats" : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED 
-          "orientation" : "landscape" // Android only (portrait|landscape), default unset so it rotates with the device 
+          "orientation" : "portrait" // Android only (portrait|landscape), default unset so it rotates with the device 
       }
    );
