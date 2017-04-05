572b4b0ccd8517d7f3657978efb2809fa625651b
4blacktop <4blogga@gmail.com>
2017-04-05 18:15:32 +1000

    1815

---
+++ php/reset.php
@@ -0,0 +1,49 @@
+<?php
+// по $qrcodeQuery
+// скрипт сбрасывает:
+// - дату первого обращения
+// - счетчик запросов
+
+ini_set("display_errors", "1");
+error_reporting(E_ALL);
+header("Access-Control-Allow-Origin:*");
+// header("Content-Type: application/json");
+echo '<pre>';
+
+$today = date("d-m-Y", time());
+echo $today . "<br />";
+
+// connection settings
+$host = "db.trueliq.com";
+$dbname = "trueliq";
+$user = "trueliq";
+$pass = "Colombia12$";
+
+$qrcodeQuery = urlencode(htmlspecialchars($_GET["qrcode"]));
+
+echo "<h1>смени хедер!!!! </h1>DEBUG:<br />fjnjkk534jn53kj6nk3jh6k2j3n6nkj3ng3d3der<br />fjnjkk534jn53kj6nk3jh6k2j3n6nkj3ng3d3det";
+
+// echo "<br /><br />QR query: $qrcode";
+// echo "<br /><br />QR result:<br />";
+
+try {  
+	$DBH = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);  
+
+	// $DBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_SILENT );
+	$DBH->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION ); // debug
+	
+	$data = array($qrcodeQuery);
+	// $STH = $DBH->prepare("UPDATE trueliq SET `date-first-query` = null `number-query` = null WHERE `qr-code` = ?");  
+	// $STH = $DBH->prepare("UPDATE trueliq SET `date-first-query` = '' `number-query` = '' WHERE `qr-code` = ?");  
+	$STH = $DBH->prepare("UPDATE trueliq SET `date-first-query` = null, `number-query` = null WHERE `qr-code` = ?");  
+	$STH->execute($data);
+
+}  
+
+catch(PDOException $e) {  
+    echo $e->getMessage();  
+	file_put_contents('PDOErrors.txt', "\r\n" . date("Ymd-His", time()) . "\t" . $e->getMessage(), FILE_APPEND); 
+}
+
+echo '</pre>';
+?>
