b0f2761415bd732442f92415cb664643fe815b19
4blacktop <4blogga@gmail.com>
2017-04-07 15:53:18 +1000

    1553

--- www/js/myapp.js
+++ www/js/myapp.js
@@ -39,6 +39,7 @@ function scan(){
 	{
           "preferFrontCamera" : false, // iOS and Android
           "showFlipCameraButton" : false, // iOS and Android
+		  "resultDisplayDuration" : 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
           // "prompt" : "Place a barcode inside the scan area", // supported on Android only
           "formats" : "QR_CODE", // default: all but PDF_417 and RSS_EXPANDED
           "orientation" : "portrait" // Android only (portrait|landscape), default unset so it rotates with the device
