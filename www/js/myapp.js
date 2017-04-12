4fffef0c401fea9492fba0d43b0f037b1ee0bac1
4blacktop <4blogga@gmail.com>
2017-04-12 15:16:20 +1000

    1516

--- www/js/myapp.js
+++ www/js/myapp.js
@@ -34,9 +34,11 @@ function scan(){
 		document.getElementById('icon').innerHTML = '';
 		cordova.plugins.barcodeScanner.scan(function(result){ // success callback
 			// document.getElementById('result').innerHTML = "Result:<br />" + result.text + "<br />" + "Format: " + result.format + "<br />" + "Cancelled: " + result.cancelled;
+			alert("Result:<br />" + result.text + "<br />" + "Format: " + result.format + "<br />" + "Cancelled: " + result.cancelled);
 			checkResult(result.text);
 		},function(error){ // error callback
 			document.getElementById('result').innerHTML = JSON.stringify(error); // send text to error function
+			$("#result").show();
 		},
 		{
 			  "preferFrontCamera" : false, // iOS and Android
